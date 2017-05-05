2fed04c2da2833d627a1d96ca67256c02cbbd82f
Brandon Ording <bording@gmail.com>
2017-05-05 18:34:59 -0400

    Add package

---
+++ GlobbingProblem/TestSuiteConstraints.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus.AcceptanceTesting.Support;
+
+namespace NServiceBus.AcceptanceTests
+{
+    public partial class TestSuiteConstraints : ITestSuiteConstraints
+    {
+        public bool SupportsDtc => true;
+
+        public bool SupportsCrossQueueTransactions => true;
+
+        public bool SupportsNativePubSub => true;
+
+        public bool SupportsNativeDeferral => true;
+
+        public bool SupportsOutbox => true;
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointInMemoryPersistence();
+
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointLearningTransport();
+    }
+}
